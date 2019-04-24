4576d143909e6e33549c06c0490841744a25fd2a
Tahsin Khan <tahsin_k@live.co.uk>
2019-04-24 11:16:45 +0100

    added a functional example to name-main

--- mod_1.py
+++ mod_1.py
@@ -1,6 +1,18 @@
 print("This is mod_1's name ->", __name__)
 
+
+def amazing_sum(number1,number2):
+    return int(number1) + int(number2)
+
+def demo_amazing_sum():
+    print("Welcome to the amazing sum DEMO!")
+    number1 = int(input("What is your first number? "))
+    number2 = int(input("What is your second number? "))
+    print(amazing_sum(number1, number2))
+
+
 if __name__ == '__main__':
+    demo_amazing_sum()
     print("We are running this file directly")
 else:
     print('This file was called/imported from somewhere')
--- mod_2.py
+++ mod_2.py
@@ -1,5 +1,6 @@
-import mod_1
+from mod_1 import *
 
 
-#print("This is mod_2's name ->" + __name__)
+print("This is mod_2's name ->" + __name__)
 
+print(amazing_sum(2, 3))
